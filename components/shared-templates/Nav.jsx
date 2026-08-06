cbf99e86e61f036ccf8c1f30905cebea435215ed
Chukwuemeka Emeche <j0she1ife31@gmail.com>
2026-08-05 21:42:51 -0700

    started working on the frontend shell

---
+++ components/shared-templates/Nav.jsx
@@ -0,0 +1,14 @@
+export default function Nav() {
+  return (
+    <>
+      {/* NAV */}
+      <nav className="site-nav">
+        <div className="site-nav-logo">Roam</div>
+        <div className="site-nav-actions">
+          <button className="btn-ghost">Log in</button>
+          <button className="btn-solid">Sign up</button>
+        </div>
+      </nav>
+    </>
+  );
+}
